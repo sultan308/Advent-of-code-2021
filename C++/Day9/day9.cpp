bfad4790458688127c8eac1bd3c1c830241ebbdc
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2021-12-09 16:04:29 +0000

    Update day9.cpp

--- C++/Day9/day9.cpp
+++ C++/Day9/day9.cpp
@@ -35,13 +35,15 @@ public:
 int findBasinSize(Point* point)
 {
     int size = 1;
+    
     //Marks the point as visited to avoid counting the same point twice;
     point->visited = true;
+    
     for(Point* adj_point : point->adjacent)
     {
         if(!adj_point->visited && adj_point->value < 9 && adj_point->value > point->value )
         {
-            size+=findBasinSize(adj_point);
+            size += findBasinSize(adj_point);
         }
     }
     
@@ -62,14 +64,17 @@ int part1(std::vector<std::vector<Point>> height_map)
 int part2(std::vector<std::vector<Point>> height_vec)
 {
     std::vector<int> sizes ;
+    
     for(std::vector<Point> row : height_vec)
     {
         for(Point p: row)
         {
-            if(p.isLowPoint())sizes.push_back(findBasinSize(&p));
+            if(p.isLowPoint()) sizes.push_back( findBasinSize(&p) ) ;
         }
     }
+    
     std::sort(sizes.begin(),sizes.end());
+    
     return (sizes[sizes.size()-3]*sizes[sizes.size()-2]*sizes[sizes.size()-1]);
 
 
@@ -102,19 +107,19 @@ int main()
 
 
         for (int i = 0; i < height_vec.size(); ++i) {
-            for (int j = 0; j < height_vec[i].size(); ++j)
+            for (int j = 0; j < height_vec[i].size() ; ++j)
             {
                 // check each offset
                 for(std::pair<int,int> off_set : off_sets)
                 {
-                    std::pair<int,int> adjasent_index = {i+off_set.first,j+off_set.second};
+                    std::pair<int,int> adjasent_index = { i + off_set.first , j + off_set.second };
 
                     //checks if the point is with in boundaries
                     if( (adjasent_index.first > -1 && adjasent_index.second > -1) &&
                       (adjasent_index.first < height_vec.size() && adjasent_index.second < height_vec[i].size()))
                     {
                         //Add the point reference to the adjacent points vector in the current point
-                        height_vec[i][j].adjacent.push_back(&height_vec[adjasent_index.first][adjasent_index.second]);
+                        height_vec[i][j].adjacent.push_back( &height_vec[ adjasent_index.first ][ adjasent_index.second ] );
 
 
                     }
